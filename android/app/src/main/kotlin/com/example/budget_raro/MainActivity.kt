2c0e03ad9a08cce6dcb796ef7bb9f8093789ad00
Stenio Oliveira <steniooliv@gmail.com>
2021-07-27 22:34:36 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/budget_raro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.budget_raro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:budget_raro/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(AppWidget());
+}
