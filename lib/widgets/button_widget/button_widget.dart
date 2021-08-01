1324473baf5bc7844ce35a5ffefa5d8da18df61d
Cícero <cicerocb2010@gmail.com>
2021-08-01 12:29:00 -0300

    Wideg_button onTap

--- lib/widgets/button_widget/button_widget.dart
+++ lib/widgets/button_widget/button_widget.dart
@@ -5,79 +5,88 @@ import 'package:budget_raro/shared/themes/text_styles.dart';
 class CustomButton extends StatelessWidget {
   final double buttonHeight;
   final double buttonWidth;
-  final String buttonText;
+  final String buttonContent;
   final IconData icon;
   final int aux;
+  final Function() onTap;
   const CustomButton({
     Key? key,
   required this.buttonHeight,
   required this.buttonWidth,
-  required this.buttonText,
+  required this.buttonContent,
   required this.icon,
   required this.aux,
+  required this.onTap,
   }) : super(key: key);
   
   const CustomButton.continuar({
     Key? key,
   this.buttonHeight = 36,
   this.buttonWidth = 114,
-  this.buttonText = 'CONTINUAR',
+  this.buttonContent = 'CONTINUAR',
   this.icon = Icons.add,
   this.aux = 3,
+  required this.onTap,
   }) : super(key: key);
 
 const CustomButton.add({
     Key? key,
   this.buttonHeight = 48,
   this.buttonWidth = 48,
-  this.buttonText = "",
+  this.buttonContent = "",
   this.icon = Icons.add,
   this.aux = 1,
+  required this.onTap,
   }) : super(key: key);
 
   const CustomButton.continuarArrow({
     Key? key,
   this.buttonHeight = 32,
   this.buttonWidth = 116,
-  this.buttonText = 'CONTINUAR  ',
+  this.buttonContent = 'CONTINUAR  ',
   this.icon = Icons.arrow_forward,
   this.aux = 1,
+  required this.onTap,
   }) : super(key: key);
 
   const CustomButton.novoControle({
     Key? key,
   this.buttonHeight = 40,
   this.buttonWidth = 182,
-  this.buttonText = '   NOVO CONTROLE',
+  this.buttonContent = '   NOVO CONTROLE',
   this.icon = Icons.add,
   this.aux = 2,
+  required this.onTap,
   }) : super(key: key);
 
   const CustomButton.tentar({
     Key? key,
   this.buttonHeight = 36,
   this.buttonWidth = 178,
-  this.buttonText = 'TENTAR NOVAMENTE',
+  this.buttonContent = 'TENTAR NOVAMENTE',
   this.icon = Icons.add,
   this.aux = 3,
+  required this.onTap,
   }) : super(key: key);
 
   const CustomButton.inserirEntrada({
     Key? key,
   this.buttonHeight = 50,
   this.buttonWidth = 123,
-  this.buttonText = '  INSERIR',
+  this.buttonContent = '  INSERIR',
   this.icon = Icons.add,
   this.aux = 2,
+  required this.onTap,
   }) : super(key: key);
 
   const CustomButton.inserirSaida({
     Key? key,
   this.buttonHeight = 50,
   this.buttonWidth = 123,
-  this.buttonText = '  INSERIR',
+  this.buttonContent = '  INSERIR',
   this.icon = Icons.add,
   this.aux = 2,
+  required this.onTap,
   }) : super(key: key);
 
 
@@ -85,17 +94,17 @@ const CustomButton.add({
   Widget build(BuildContext context) {
     List<Widget> content =[];
     if (aux == 1) {
-      content =[Text(buttonText, style: TextStyles.textButton,), Icon(icon, color: Colors.white,)];
+      content =[Text(buttonContent, style: TextStyles.textButton,), Icon(icon, color: Colors.white,)];
     }
     if (aux == 2){
-      content =[Icon(icon, color: Colors.white,), Text(buttonText, style: TextStyles.textButton,)];
+      content =[Icon(icon, color: Colors.white,), Text(buttonContent, style: TextStyles.textButton,)];
     }
     if (aux == 3) {
-      content =[Text(buttonText, style: TextStyles.textButton,)];
+      content =[Text(buttonContent, style: TextStyles.textButton,)];
     }
     
     return InkWell(
-      onTap: () {},
+      onTap: onTap,
       child: Container(
         height: buttonHeight,
         width: buttonWidth,
