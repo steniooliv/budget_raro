40ed12b6fbbab8e37f97c0d7afb51b6dd48db6be
Stenio Oliveira <steniooliv@gmail.com>
2021-08-21 10:09:56 -0300

    lock device orientation

--- lib/modules/app_widget.dart
+++ lib/modules/app_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Budget',
