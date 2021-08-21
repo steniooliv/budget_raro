6fc18755b596699d7958769271f86cfd7de49b18
Stenio Oliveira <steniooliv@gmail.com>
2021-08-21 19:07:23 -0300

    firebase repository update create_accounting

--- lib/shared/auth/firebase_authenticate.dart
+++ lib/shared/auth/firebase_authenticate.dart
@@ -1,9 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:budget_raro/shared/auth/firebase_repository.dart';
-import 'package:budget_raro/shared/models/user_model.dart';
 
 class FirebaseAuthenticate {
   final FirebaseRepository firebaseRepository;
@@ -11,4 +6,11 @@ class FirebaseAuthenticate {
   FirebaseAuthenticate({
     required this.firebaseRepository,
   });
+
+  Future<void> login() async {
+    //TODO: CHAMAR O FIREBASE AUTH, PASSAR O EMAIL E SENHA
+    //RETORNAR O USUÁRIO E CHAMAR A FUNÇÃO DE SALVAR USUÁRIO NO SHARED PREFERENCES
+    //firebaseRepository.saveUser(user)
+    //MANDO O USUÁRIO PRA HOMEPAGE
+  }
 }
--- lib/shared/auth/firebase_repository.dart
+++ lib/shared/auth/firebase_repository.dart
@@ -1,10 +1,7 @@
-import 'dart:io';
-
 import 'package:budget_raro/shared/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 enum FirebaseStatus {
@@ -37,22 +34,40 @@ class FirebaseRepository {
   Future<void> saveUser(UserModel user) async {
     final instance = await SharedPreferences.getInstance();
     await instance.setString('user', user.toJson());
+    print("${instance.getString('user')}");
     return;
   }
 
-  Future<void> createAccounting({email, password}) async {
+  Future<void> createAccounting({
+    required String name,
+    required String email,
+    required String cpf,
+    required String phone,
+    required String password,
+  }) async {
     try {
       UserCredential userCredential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
       final _uid = userCredential.user!.uid;
       final _email = userCredential.user!.email!;
+      final _name = name;
+      final _cpf = cpf;
+      final _phone = phone;
       await firebaseFirestore.collection('users').doc('$_uid').set({
+        'name': _name,
         'email': _email,
+        'cpf': _cpf,
+        'phone': _phone,
+        'created_at': FieldValue.serverTimestamp(),
       });
-      final user = UserModel(email: _email, uid: _uid);
+
+      final user = UserModel(
+          email: _email, uid: _uid, cpf: _cpf, name: _name, phone: _phone);
       saveUser(user);
-    } catch (e) {
-      print(e);
+    } on FirebaseException catch (e) {
+      if (e.code == 'email-already-in-use') {
+        print('This e-mail already exist in other user accouting.');
+      }
     }
   }
 }
