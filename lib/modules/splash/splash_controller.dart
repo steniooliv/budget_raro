9f5f0e807c013ff97e61d643e8ab3f5597d77391
Stenio Oliveira <steniooliv@gmail.com>
2021-08-20 21:34:58 -0300

    initialize firebase on splash for example

--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -1,7 +1,15 @@
+import 'package:budget_raro/shared/auth/firebase_repository.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class SplashController {
+part 'splash_controller.g.dart';
+
+class SplashController = _SplashControllerBase with _$SplashController;
+
+abstract class _SplashControllerBase with Store {
+  late final FirebaseRepository firebaseRepository = FirebaseRepository();
+
   Future<void> hasUser() async {
     await Future.delayed(Duration(seconds: 3));
     SharedPreferences prefs = await SharedPreferences.getInstance();
