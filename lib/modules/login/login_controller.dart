a1f088539433811f86e7642c72bb35587b33cd95
Cícero <cicerocb2010@gmail.com>
2021-08-22 00:14:56 -0300

    backend/#60-login-page

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,7 +1,74 @@
+import 'package:budget_raro/shared/auth/firebase_authenticate.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:mobx/mobx.dart';
-
+import 'package:budget_raro/shared/auth/firebase_repository.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 part 'login_controller.g.dart';
+
 
 class LoginController = _LoginControllerBase with _$LoginController;
 
-abstract class _LoginControllerBase with Store {}
+abstract class _LoginControllerBase with Store {
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  
+  FirebaseRepository firebaseRepository = FirebaseRepository();
+
+  
+  
+  @action
+  Future<void> firebaseInitialize() async {
+    Modular.get<FirebaseRepository>();
+    await FirebaseRepository().firebaseInitialize();
+  }
+  
+  @observable
+  PageController pageController =PageController(initialPage: 0);
+
+  @observable
+  final formKey = GlobalKey<FormState>();
+  
+  @action
+  nextPage() {
+    if (pageController.hasClients && formKey.currentState!.validate()) {
+      pageController.nextPage(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+  @action
+  prevPage() {
+    if (pageController.hasClients && formKey.currentState!.validate()) {
+      pageController.previousPage(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+      );
+    }
+  }
+  @action
+  ontapAux() {}
+
+  @action
+  toRegisterPage() {
+    Modular.to.pushNamed('/login/create_accounting');
+  }
+
+  @action
+  toHomePage() {
+    FirebaseAuthenticate firebaseAuthenticate = FirebaseAuthenticate(firebaseRepository: firebaseRepository);
+    if (formKey.currentState!.validate())  {
+      firebaseAuthenticate
+      .login(
+        email: email.text,
+        password: password.text,
+      ).then(
+        (value) {
+          pageController.jumpToPage(0);
+          Modular.to.navigate('/home');
+        },
+      );
+    }
+  }
+  
+}
